f4cd68d39e6bca7f879eb09538513ddb4481d6bf
GeneKung <52974083+GeneKung@users.noreply.github.com>
2023-09-01 12:29:21 -0700

    first commit

---
+++ task-manager-backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost/task-manager', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Task Manager API');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
